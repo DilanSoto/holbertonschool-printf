872f11c3abf34a97c852ed5541a4e50f1fc02152
DilanSoto <144055652+DilanSoto@users.noreply.github.com>
2023-11-27 01:46:39 -0400

    Update functions.c

--- functions.c
+++ functions.c
@@ -79,7 +79,44 @@ int _print_num(va_list args, int uppercase)
  */
 int _print_nint(va_list args)
 {
-	return _print_num(args, 10, 0);
+	int _print_nint(va_list ap)
+{
+	int a[10];
+	int j, m, n, sum, count;
+
+	n = va_arg(ap, int);
+	count = 0;
+	m = 1000000000;
+
+	a[0] = n / m;
+
+	for (j = 1; j < 10; j++)
+	{
+		m /= 10;
+		a[j] = (n / m) % 10;
+	}
+
+	if (n < 0)
+	{
+		_putchar('-');
+		count++;
+		for (j = 0; j < 10; j++)
+		{
+			a[j] *= -1;
+		}
+	}
+
+	for (j = 0, sum = 0; j < 10; j++)
+	{
+		sum += a[j];
+
+		if (sum != 0 || j == 9)
+		{
+			_putchar('0' + a[j]);
+			count++;
+		}
+	}
+	return (count);
 }
 
 /**

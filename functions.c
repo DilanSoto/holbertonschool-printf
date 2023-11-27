440792af21715b02a47f4c696e0186075d05fc05
DilanSoto <144055652+DilanSoto@users.noreply.github.com>
2023-11-27 10:25:10 -0400

    Update functions.c

--- functions.c
+++ functions.c
@@ -1,122 +1,152 @@
 #include "main.h"
-#include <string.h>
-
-int _write_char(char ch) {
-    return (write(1, &ch, 1));
+#include <string.h> /**
+* _write_char - writes the character c to stdout
+* @c: the character to print
+*
+* Return: the number of characters printed (always 1).
+*/
+int _write_char(char c)
+{
+	return (write(1, &c, 1));
+}
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * Return: On success 1.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
 }
 
-int _putchar(char ch) {
-    return (write(1, &ch, 1));
+/**
+ * print_char - prints a character to stdout
+ * @ap: the arguments pointer
+ *
+ * Return: the number of characters printed (always 1).
+ */
+int _print_char(va_list ap)
+{
+	int buffer = va_arg(ap, int);
+
+	write(1, &buffer, 1);
+	return (1);
 }
 
-int _print_char(va_list args) {
-    int buffer = va_arg(args, int);
-    return (_write_char(buffer));
+/**
+ * print_string - prints a string to stdout
+ * @ap: the argument pointer
+ *
+ * Return: the number of characters printed
+ */
+int _print_string(va_list ap)
+{
+	char *str;
+
+	str = va_arg(ap, char *);
+
+	if (str == NULL)
+		str = "(null)";
+	return (write(1, str, strlen(str)));
 }
 
-int _print_string(va_list args) {
-    char *str = va_arg(args, char *);
-    if (str == NULL)
-        str = "(null)";
-    return (write(1, str, strlen(str)));
+/**
+ * _print_percent - prints a percent symbol to stdout
+ * @ap: the argument pointer
+ * Return: the number of characters printed
+ */
+int _print_percent(va_list ap)
+{
+	(void)ap;
+	return (_write_char('%'));
 }
 
-int _print_percent(va_list args) {
-    (void)args;
-    return (_write_char('%'));
+/**
+ * print_i - Prints an integer.
+ * @i: The integer to print.
+ *
+ * Return: Chars and digits printed in to stdout.
+ */
+int _print_int(va_list ap)
+{
+	int a[10];
+	int j, m, n, sum, count;
+
+	n = va_arg(ap, int);
+	count = 0;
+	m = 1000000000;
+
+	a[0] = n / m;
+
+	for (j = 1; j < 10; j++)
+	{
+		m /= 10;
+		a[j] = (n / m) % 10;
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		count++;
+		for (j = 0; j < 10; j++)
+		{
+			a[j] *= -1;
+		}
+	}
+	for (j = 0, sum = 0; j < 10; j++)
+	{
+		sum += a[j];
+
+		if (sum != 0 || j == 9)
+		{
+			_putchar('0' + a[j]);
+			count++;
+		}
+	}
+	return (count);
 }
 
-int _print_int(va_list args) {
-    return _print_num(args, 0);
-}
+/**
+ * print_d - Prints the integer of a decimal.
+ * @d: Decimal to print.
+ *
+ * Return: Chars and digits printed in to the stdout.
+ */
+int _print_nint(va_list ap)
+{
+	int a[10];
+	int j, m, n, sum, count;
 
-int _print_nint(va_list args) {
-    return _print_num(args, 0);
-}
+	n = va_arg(ap, int);
+	count = 0;
+	m = 1000000000;
 
-int _print_num(va_list args, int uppercase) {
-    int digits[10], j, m, num, sum, count = 0;
-    num = va_arg(args, int);
-    m = 1000000000;
-    digits[0] = num / m;
+	a[0] = n / m;
 
-    for (j = 1; j < 10; j++) {
-        m /= 10;
-        digits[j] = (num / m) % 10;
-    }
-    if (num < 0) {
-        _putchar('-');
-        count++;
-        for (j = 0; j < 10; j++)
-            digits[j] *= -1;
-    }
-    for (j = 0, sum = 0; j < 10; j++) {
-        if (sum != 0 || j == 9) {
-            char ch = uppercase ? '0' + digits[j] - 32 : '0' + digits[j];
-            count += _write_char(ch);
-        }
-        sum += digits[j];
-    }
-    return count;
-}
-#include "main.h"
-#include <string.h>
+	for (j = 1; j < 10; j++)
+	{
+		m /= 10;
+		a[j] = (n / m) % 10;
+	}
 
-int _write_char(char ch) {
-    return (write(1, &ch, 1));
-}
+	if (n < 0)
+	{
+		_putchar('-');
+		count++;
+		for (j = 0; j < 10; j++)
+		{
+			a[j] *= -1;
+		}
+	}
 
-int _putchar(char ch) {
-    return (write(1, &ch, 1));
-}
+	for (j = 0, sum = 0; j < 10; j++)
+	{
+		sum += a[j];
 
-int _print_char(va_list args) {
-    int buffer = va_arg(args, int);
-    return (_write_char(buffer));
-}
-
-int _print_string(va_list args) {
-    char *str = va_arg(args, char *);
-    if (str == NULL)
-        str = "(null)";
-    return (write(1, str, strlen(str)));
-}
-
-int _print_percent(va_list args) {
-    (void)args;
-    return (_write_char('%'));
-}
-
-int _print_int(va_list args) {
-    return _print_num(args, 0);
-}
-
-int _print_nint(va_list args) {
-    return _print_num(args, 0);
-}
-
-int _print_num(va_list args, int uppercase) {
-    int digits[10], j, m, num, sum, count = 0;
-    num = va_arg(args, int);
-    m = 1000000000;
-    digits[0] = num / m;
-
-    for (j = 1; j < 10; j++) {
-        m /= 10;
-        digits[j] = (num / m) % 10;
-    }
-    if (num < 0) {
-        _putchar('-');
-        count++;
-        for (j = 0; j < 10; j++)
-            digits[j] *= -1;
-    }
-    for (j = 0, sum = 0; j < 10; j++) {
-        if (sum != 0 || j == 9) {
-            char ch = uppercase ? '0' + digits[j] - 32 : '0' + digits[j];
-            count += _write_char(ch);
-        }
-        sum += digits[j];
-    }
-    return count;
+		if (sum != 0 || j == 9)
+		{
+			_putchar('0' + a[j]);
+			count++;
+		}
+	}
+	return (count);
 }
